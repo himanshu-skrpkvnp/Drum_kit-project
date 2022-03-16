a2346100ee73d6ccc68efb73b122931275776f21
Himanshu Singh <himanshusinghskrpkvnp@Gmail.com>
2022-03-16 21:30:43 +0530

    added keyboard keys event listener

--- index.js
+++ index.js
@@ -4,77 +4,49 @@ for( var i = 0 ; i <= document.querySelectorAll(".drum").length ; i++ ) {
     document.querySelectorAll("button")[i].addEventListener("click" , function handleClick()
     {
       var buttonInnerHTML = this.innerHTML ;
-      switch (buttonInnerHTML) {
-          case "w":
-                var tom1 = new Audio("sounds/tom-1.mp3");
-                tom1.play() ;
-                break;
-          case "a":
-                var tom2 = new Audio("sounds/tom-2.mp3");
-                tom2.play() ;
-                break;
-         case "s":
-                var tom3 = new Audio("sounds/tom-3.mp3");
-                tom3.play() ;
-                break;
-         case "d":
-                var tom4 = new Audio("sounds/tom-4.mp3");
-                tom4.play() ;
-                break;
-         case "j":
-                var snare = new Audio("sounds/snare.mp3");
-                snare.play() ;
-                break;
-         case "k":
-                var kickbass = new Audio("sounds/kick-bass.mp3");
-                kickbass.play() ;
-                break;
-         case "l":
-                var crash = new Audio("sounds/crash.mp3");
-                crash.play() ;
-                break;
-      
-          default: console.log(buttonInnerHTML)
-              break;
-      }
+      makeSound(buttonInnerHTML) ;
 
     });
 }
-// adding keypress event listener to all the keys of the keyboard
-document.addEventListener( "keypress" , function()
+// Detecting keyboard press
+document.addEventListener('keydown',function(event){
+    makeSound(event.key);
+});
+
+
+//  detecting which key is pressed and producing subsequent tune
+function makeSound(key)
 {
-    alert(" key ha sbeen pressed ");
-} )  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// var audio = new Audio("sounds/tom-1.mp3");
-// audio.play() ;
+    switch (key) {
+        case "w":
+              var tom1 = new Audio("sounds/tom-1.mp3");
+              tom1.play() ;
+              break;
+        case "a":
+              var tom2 = new Audio("sounds/tom-2.mp3");
+              tom2.play() ;
+              break;
+       case "s":
+              var tom3 = new Audio("sounds/tom-3.mp3");
+              tom3.play() ;
+              break;
+       case "d":
+              var tom4 = new Audio("sounds/tom-4.mp3");
+              tom4.play() ;
+              break;
+       case "j":
+              var snare = new Audio("sounds/snare.mp3");
+              snare.play() ;
+              break;
+       case "k":
+              var kickbass = new Audio("sounds/kick-bass.mp3");
+              kickbass.play() ;
+              break;
+       case "l":
+              var crash = new Audio("sounds/crash.mp3");
+              crash.play() ;
+              break;
+    
+        default: console.log(buttonInnerHTML)
+            break;
+           }}
